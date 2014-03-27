c723275303c34dde9fe8919d863e8124ce9de882
edsykes <ed.sykes.public@gmail.com>
2014-03-27 14:13:26 +0000

    Created this example

---
+++ HookIntoBuildEvents/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.HookIntoBuildEvents
+{
+    static class GuidList
+    {
+        public const string guidHookIntoBuildEventsPkgString = "535cbb18-71d2-4190-9c04-b8077c87799d";
+        public const string guidHookIntoBuildEventsCmdSetString = "c50fb76f-d8d6-41d3-8668-04aa1987d599";
+
+        public static readonly Guid guidHookIntoBuildEventsCmdSet = new Guid(guidHookIntoBuildEventsCmdSetString);
+    };
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Hook into visual studio build events
+========================================
+
+This is an example of how to build a VSPackage and hook into build events. It simply logs the build events being triggered. It also shows how to automatically load the VSPackage when a solution is added to the UI.
+
